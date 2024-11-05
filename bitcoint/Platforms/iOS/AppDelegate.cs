27597662f0a07796f0754fb83b1180c483efad8a
xHelpy <xiozeerpl@gmail.com>
2024-11-05 13:06:05 +0100

    Adding files to the repository

---
+++ bitcoint/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace bitcoint
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
